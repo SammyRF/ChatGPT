e43158c0c0e0d16fdff6f8591ff372db9bf22d6b
428 <rong.fan@live.com>
2023-07-16 17:48:50 +0200

    add simple chatbot

---
+++ chatBot.py
@@ -0,0 +1,23 @@
+import os
+import openai 
+import gradio as gr
+import keys
+
+openai.api_key = keys.CHATGPT_API_KEY
+
+messages = [
+    {'role': 'system', 'content': 'You are a helpful AI assistant.'},
+]
+
+def chat(user_input):
+    if user_input:
+        messages.append({'role': 'user', 'content': user_input})
+        response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages = messages)
+        reply = response.choices[0].message.content
+        messages.append({'role': 'assistant', 'content': reply})
+        return reply
+    
+inputs = gr.Textbox(label='User Input')
+outputs = gr.Textbox(label='Response')
+gr.Interface(fn=chat, inputs=inputs, outputs=outputs, title='ChatGPT Bot').launch(share=True)
+
